9c4d387e48ef8c835155e07ff6a97f334e9f9d4a
John Lilly <jm2lilly@gmail.com>
2025-03-03 21:01:12 -0500

    Reduce slider speed

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -3,5 +3,6 @@ const slider = new A11YSlider(document.querySelector('.slider'), {
     arrows: true,
     dots: false,
     autoplay: true,
-    slidesToShow: 1
+    slidesToShow: 1,
+    autoplaySpeed: 6000
 });
